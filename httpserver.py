dffa35d3b1dfdc2cc1cc84809408572cba158c4b
Your Name <you@example.com>
2025-04-08 21:41:49 +0200

    adam chce

--- httpserver.py
+++ httpserver.py
@@ -14,7 +14,6 @@ def readline(client_socket: socket.socket) -> str:
             break
 
         buffer += byte
-        # print(f"Bajt:{byte}")
         if byte == b"\n":
 
             break
@@ -27,14 +26,11 @@ def handle_connection(client_socket: socket.socket):
         while True:
             request_line = readline(client_socket).strip()
             if request_line == "":
-                # print("mame vse.")
                 break
             radky.append(request_line)
-            # print("radka:"+request_line)
         
                     
         status_code = "200 OK"
-        # print(f"Received: {request_line}")
         prvni_radka = radky[0].split(" ")
         if len(prvni_radka)!=3:
             pass    
@@ -53,9 +49,8 @@ def handle_connection(client_socket: socket.socket):
             odpovidani(status_code, odpoved,"text/html")
             return 
 
-        verze = prvni_radka[2]
         
-        trash, extension = os.path.splitext(cesta)
+        extension = os.path.splitext(cesta)[1]
         content_type = koncovky[extension]
         try:
             with open(cesta,"rb") as soubor:
@@ -78,13 +73,11 @@ def handle_connection(client_socket: socket.socket):
 
     odpovidani(status_code,odpoved,content_type)
 
-    # odpoved = f"metoda: {metoda}\ncesta: {cesta}\nverze: {verze}\n" 
 
 
 
 def odpovidani(status_code, odpoved,content_type):
 
-    # print(f"Metoda: {metoda}\nCesta: {cesta}\nVerze: {verze}")
     client_socket.send(f"HTTP/1.1 {status_code}\r\nContent-Type: {content_type}\r\nX-Powered-By: FIKS HTTPSERVER\r\nContent-Length: {len(odpoved)}\r\n\r\n".encode("utf-8"))
 
     client_socket.send(odpoved)
@@ -95,21 +88,17 @@ def odpovidani(status_code, odpoved,content_type):
 
     
 
-# create a socket object
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_ip = "0.0.0.0"
 port = 8000
 
-# bind the socket to a specific address and port
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 server.bind((server_ip, port))
 
-# listen for incoming connections
 server.listen(0)
 print(f"Listening on {server_ip}:{port}")
 try:
     while True:
-        # accept incoming connections
         client_socket, client_address = server.accept()
         print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
 
